20604027a05a990ef674ac03968efa6dc9270d81
KJZH001 <2802007619@qq.com>
2021-01-20 12:39:37 +0800

    [更新]修改css添加于cdn中

--- pages/panel.php
+++ pages/panel.php
@@ -17,11 +17,11 @@ $rs = Database::querySingleLine("users", Array("username" => $_SESSION['user']))
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<title>管理面板 :: <?php echo $_config[ 'sitename']; ?> - <?php echo $_config[ 'description']; ?></title>
 		<!-- Font Awesome Icons -->
-		<link rel="stylesheet" href="assets/panel/plugins/fontawesome-free/css/all.min.css">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/KJZH001/MoeSakuraPanel/assets/panel/plugins/fontawesome-free/css/all.min.css">
 		<!-- IonIcons -->
 		<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 		<!-- Theme style -->
-		<link rel="stylesheet" href="assets/panel/dist/css/adminlte.min.css">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/KJZH001/MoeSakuraPanel/assets/panel/dist/css/adminlte.min.css">
 		<!-- Google Font: Source Sans Pro -->
 		<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet"></head>
 	
@@ -179,11 +179,11 @@ $rs = Database::querySingleLine("users", Array("username" => $_SESSION['user']))
 		<!-- ./wrapper -->
 		<!-- REQUIRED SCRIPTS -->
 		<!-- jQuery -->
-		<script src="assets/panel/plugins/jquery/jquery.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/gh/KJZH001/MoeSakuraPanel/assets/panel/plugins/jquery/jquery.min.js"></script>
 		<!-- Bootstrap -->
-		<script src="assets/panel/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/gh/KJZH001/MoeSakuraPanel/assets/panel/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 		<!-- AdminLTE -->
-		<script src="assets/panel/dist/js/adminlte.js"></script>
+		<script src="https://cdn.jsdelivr.net/gh/KJZH001/MoeSakuraPanel/assets/panel/dist/js/adminlte.js"></script>
 	</body>
 
 </html>
